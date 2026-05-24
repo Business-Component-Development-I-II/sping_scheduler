9eb0297049eb2cb9e327ee7c3effc81f0c58d696
kalharaweragoda <kalharaweragoda@gmail.com>
2026-05-24 15:45:11 +0530

    log

--- src/main/java/edu/icet/scheduler/SmsScheduler.java
+++ src/main/java/edu/icet/scheduler/SmsScheduler.java
@@ -1,16 +1,21 @@
 package edu.icet.scheduler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SmsScheduler {
 
+    private static final Logger log = LoggerFactory.getLogger(SmsScheduler.class);
+
     @Scheduled(cron = "* * * * * *")
     void sendSms(){
         //DB call to grt Customer List
         //Extract Mobile Numbers
         //SMS message
-        System.out.println("SMS sent");
+        //System.out.println("SMS sent");
+        log.info("SMS Sent");
     }
 }
